eb985934b777814c88ad9664e62f943df00549a8
Artem Zhirkov <az@develdynamic.com>
2013-08-28 13:51:39 +0400

    introducing Volume class for disk images

---
+++ src/main/java/net/elbandi/pve2api/data/Volume.java
@@ -0,0 +1,16 @@
+package net.elbandi.pve2api.data.resource;
+
+import net.elbandi.pve2api.data.Resource;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: artemz
+ * Date: 8/28/13
+ * Time: 1:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Volume {
+	public Volume(){
+
+	}
+}
